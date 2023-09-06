042f8a274c7036da978052e7e4b5aad412db675d
galib <mgalib101@gmail.com>
2023-09-06 09:40:54 +0600

    add: readme file updated

--- url.js
+++ url.js
@@ -1,3 +1,3 @@
-// export  const  L_URL='http://localhost:3000'
-export  const  L_URL='https://crud-app-by-next-js.vercel.app'
+export  const  L_URL='http://localhost:3000'
+// export  const  L_URL='https://crud-app-by-next-js.vercel.app'
 // https://crud-app-by-next-js.vercel.app
